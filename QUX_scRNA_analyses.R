3afdf5cb9f1aea4de59dd67bf7464ef18880e2d3
Yang Li <liyang@pitzer-rw02.ten.osc.edu>
2022-06-10 23:05:25 -0400

    Perform dimension reduction via rPCA

--- QUX_scRNA_analyses.R
+++ QUX_scRNA_analyses.R
@@ -13,47 +13,81 @@ work.dir <- "/fs/ess/PCON0022/liyang/astrazeneca/QUX/20220524_DITQUX_145_INSERM_
 library(Seurat)
 
 
-# Load data into matrices
-dir.list <- list.dirs(work.dir)
-dir.list
-length(dir.list)
-dir.list <- dir.list[c(3, 5, seq(7, 49, by = 3))]
-length(dir.list)
-ifnb.list <- c()
-for (i in seq_along(dir.list)) {
-  message ("Loading the ", i, "-th matrix ...\n")
-  ifnb.list[[i]] <- CreateSeuratObject(counts = Seurat::Read10X(dir.list[i]),
-                                    project = "QUX", min.cells = 3, min.features = 200)
-}
-library(qs)
-qs::qsave(ifnb.list, paste0(work.dir, "Object_list.qsave"))
+# # Load data into matrices
+# dir.list <- list.dirs(work.dir)
+# dir.list
+# length(dir.list)
+# dir.list <- dir.list[c(3, 5, seq(7, 49, by = 3))]
+# length(dir.list)
+# ifnb.list <- c()
+# for (i in seq_along(dir.list)) {
+#   message ("Loading the ", i, "-th matrix ...\n")
+#   ifnb.list[[i]] <- CreateSeuratObject(counts = Seurat::Read10X(dir.list[i]),
+#                                     project = "QUX", min.cells = 3, min.features = 200)
+# }
+# library(qs)
+# qs::qsave(ifnb.list, paste0(work.dir, "Object_list.qsave"))
+ifnb.list <- qs::qread(paste0(work.dir, "Object_list.qsave"))
 
 
-# Size of all matrices
-sapply(ifnb.list, dim)
-sum(sapply(ifnb.list, ncol))
+# # Size of all matrices
+# sapply(ifnb.list, dim)
+# sum(sapply(ifnb.list, ncol))
 
 
-# Performing integration on datasets normalized with SCTransform
-ifnb.list <- lapply(X = ifnb.list, FUN = SCTransform)
-features <- SelectIntegrationFeatures(object.list = ifnb.list, nfeatures = 3000)
-qs::qsave(features, paste0(work.dir, "Integration_features.qsave"))
-ifnb.list <- PrepSCTIntegration(object.list = ifnb.list, anchor.features = features)
-qs::qsave(ifnb.list, paste0(work.dir, "Object_list.qsave"))
-# ifnb.list <- qs::qread(paste0(work.dir, "Object_list.qsave"))
+# # Performing integration on datasets normalized with SCTransform
+# ifnb.list <- lapply(X = ifnb.list, FUN = SCTransform)
 
 
+# After acquiring the data, we first perform standard normalization and variable feature selection.
+library(future)
+library(future.apply)
+plan("multicore", workers = 4)
+options(future.globals.maxSize = 20000 * 1024^2)
+ifnb.list <- lapply(X = ifnb.list, FUN = function(x) {
+  x <- NormalizeData(x, verbose = FALSE)
+  x <- FindVariableFeatures(x, verbose = FALSE)
+})
+qs::qsave(ifnb.list, paste0(work.dir, "Normalized_HVG_object_list.qsave"))
 
-immune.anchors <- FindIntegrationAnchors(object.list = ifnb.list, normalization.method = "SCT",
-                                         anchor.features = features)
-qs::qsave(immune.anchors, paste0(work.dir, "Integration_anchors.qsave"))
-immune.combined.sct <- IntegrateData(anchorset = immune.anchors, normalization.method = "SCT")
-qs::qsave(immune.combined.sct, paste0(work.dir, "Integrated_object.qsave"))
+
+# features <- SelectIntegrationFeatures(object.list = ifnb.list, nfeatures = 3000)
+# qs::qsave(features, paste0(work.dir, "Integration_features.qsave"))
+# ifnb.list <- PrepSCTIntegration(object.list = ifnb.list, anchor.features = features)
+# qs::qsave(ifnb.list, paste0(work.dir, "Object_list.qsave"))
+# # ifnb.list <- qs::qread(paste0(work.dir, "Object_list.qsave"))
 
 
-immune.combined.sct <- RunPCA(immune.combined.sct, verbose = FALSE)
-immune.combined.sct <- RunUMAP(immune.combined.sct, reduction = "pca", dims = 1:30)
-qs::qsave(immune.combined.sct, paste0(work.dir, "Integrated_object.qsave"))
+# Next, select features for downstream integration, and run PCA on each object 
+# in the list, which is required for running the reciprocal PCA workflow.
+features <- SelectIntegrationFeatures(object.list = ifnb.list)
+ifnb.list <- lapply(X = ifnb.list, FUN = function(x) {
+  x <- ScaleData(x, features = features, verbose = FALSE)
+  x <- RunPCA(x, features = features, verbose = FALSE)
+})
+qs::qsave(ifnb.list, paste0(work.dir, "PCA_object_list.qsave"))
+
+
+# rPCA integration
+anchors <- FindIntegrationAnchors(object.list = ifnb.list, reference = c(1, 2), reduction = "rpca", 
+                                  dims = 1:30)
+ifnb.integrated <- IntegrateData(anchorset = anchors, dims = 1:30)
+qs::qsave(ifnb.integrated, paste0(work.dir, "rPCA_integrated_object.qsave"))
+
+
+# # CCA integration
+# immune.anchors <- FindIntegrationAnchors(object.list = ifnb.list, normalization.method = "SCT",
+#                                          anchor.features = features)
+# qs::qsave(immune.anchors, paste0(work.dir, "Integration_anchors.qsave"))
+# immune.anchors <- qs::qread(paste0(work.dir, "Integration_anchors.qsave"))
+# immune.combined.sct <- IntegrateData(anchorset = immune.anchors, normalization.method = "SCT")
+# qs::qsave(immune.combined.sct, paste0(work.dir, "Integrated_object.qsave"))
+
+
+# Downstream analyses
+ifnb.integrated <- RunPCA(ifnb.integrated, verbose = FALSE)
+ifnb.integrated <- RunUMAP(ifnb.integrated, reduction = "pca", dims = 1:30)
+qs::qsave(ifnb.integrated, paste0(work.dir, "PCA_UMAP_rPCA_integrated_object.qsave"))
 
 
 # p1 <- DimPlot(immune.combined.sct, reduction = "umap", group.by = "stim")
--- Run_integration_dim_reduction.sh
+++ Run_integration_dim_reduction.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --job-name=AZ_QUX_scRNA-Seq_analyses
-#SBATCH --time=36:20:59
+#SBATCH --time=24:20:59
 #SBATCH --output=AZ_QUX_scRNA-Seq_analyses.out
 #SBATCH --account=PCON0022
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=8
-#SBATCH --mem=300GB
+#SBATCH --mem=500GB
 #SBATCH --gpus-per-node=1
 
 
